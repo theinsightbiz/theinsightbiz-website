62df246c86cab88859657470a07a6d737615fea9
Pratik <theinsightbiz@gmail.com>
2025-09-27 01:42:10 +0530

    fixed the image fuck ups

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 export default function Hero(){
   const navigate = useNavigate()
   return (
-    <section className="hero" style={{backgroundImage: 'url(/src/assets/laptop-hero.png)'}}>
+    <section className="hero" style={{backgroundImage: 'url(/assets/feedback/laptop-hero.png)'}}>
       <div className="hero-overlay">
         <h1 className="hero-title">Insight Beyond Numbers, <br/> Excellence Beyond Limits</h1>
         <div className="hero-ctas">
